e7c951691c2c2615e66bff06f7152733649a4cda
Malcon-25 <cowaya24@gmail.com>
2024-02-06 17:24:26 -0600

    Firts commit

---
+++ app-primera/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+    let username = '';
+    let email = '';
+    let guardar = false;
+  
+    const handleSave = async () => {
+      
+      console.log('Guardando datos:', { username, email });
+    };
+  </script>
+  
+  {#if guardar}
+    <div>
+      <p>Bienvenido, {username}!</p>
+      <button on:click={() => (guardar = false)}>Cerrar sesión</button>
+    </div>
+  {:else}
+    <form>
+      <label>
+        Usuario:
+        <input type="text" bind:value={username} />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input type="email" bind:value={email} />
+      </label>
+      <br />
+      <button type="button" on:click={handleSave}>Guardar</button>
+    </form>
+  {/if}
+  
